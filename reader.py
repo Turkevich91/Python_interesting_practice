385f7e0e41d37ec95021d46fd4c46a450b8831ec
Turkevich91 <vetalturkevich@gmail.com>
2020-04-06 12:53:10 -0400

    first lesson Yes NO parser

--- reader.py
+++ reader.py
@@ -1,22 +1,22 @@
-def man():
+# def man():
     #read file
-    file = open("yesno.txt", "r")
-    lines = file.readlines()
-    file.close()
+file = open("yesno.txt", "r")
+lines = file.readlines()
+file.close()
 
-    #look for patterns
-    countYes = 0
-    countNo = 0
-    for line in lines:
-        line = line.strip().upper()
-        #print(line)
-        if line.find("YES") != -1:
-            countYes = countYes + 1
-        if line.find("NO") != -1:
-            countNo = countNo +1
+#look for patterns
+countYes = 0
+countNo = 0
+for line in lines:
+    line = line.strip().upper()
+    #print(line)
+    if line.find("YES") != -1:
+        countYes = countYes + 1
+    if line.find("NO") != -1:
+        countNo = countNo +1
 
-        #display result
-    print("Yes: ", countYes)
-    print("No: ", countNo)
+    #display result
+print("Yes: ", countYes)
+print("No: ", countNo)
 
-man()
+# man()

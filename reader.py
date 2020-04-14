4baf688133715bdc45e80eaa3b598ae9a8200241
Turkevich91 <vetalturkevich@gmail.com>
2020-04-14 09:23:17 -0400

    File mass renaming v2

--- reader.py
+++ reader.py
@@ -1,6 +1,7 @@
 import os
 
-os.chdir(r'P:\2019\19042\PAP 04') #получает список файлов в папке
+print(os.environ)
+os.chdir(r'P:\_Programming_JOBS\_Turret Punch_\2019\19042\PAP 04') #получает список файлов в папке
 
 for f in os.listdir():
     file_name, file_ext = os.path.splitext(f)
@@ -9,4 +10,7 @@ for f in os.listdir():
     f_job = f_job.strip()
     f_release = f_release.strip()
     f_num = f_num.strip()[1:].zfill(2)
-    print('{}-{}-{}{}'.format(f_num, f_release, f_job, file_ext))
+    print('{}-{}{}'.format(f_job, f_num, file_ext))
+
+
+

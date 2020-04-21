28bae6a0abc87d842a6c7779aaab06a2c20b2b8a
Turkevich91 <vetalturkevich@gmail.com>
2020-04-21 13:01:28 -0400

    so far so good

--- filesCopier.py
+++ filesCopier.py
@@ -1,12 +1,21 @@
 import os
 
+projectsRoot = r"\\mcp-fsvs2\Engineering\01 Projects"
+
 print('write work and release number using through space separator:')
-w = '1873 PAP14'.split()  # w = input().split()
-print(w)
-year = '20' + w[0][:2]  # 2018  (first 2 digits represent year)
-print(year)
-srcProjectRootPath = r"\\mcp-fsvs2\Engineering\01 Projects" + '\\' + year   # this action requires domain permission
+jobN, releaseN = '1873 PAP14'.split()  # w = input().split()
+print('Job# ', jobN, 'Release: ', releaseN)
+year = '20' + jobN[:2]  # 2018  (first 2 digits represent year)
+print('Year: ', year)
+srcProjectRootPath = projectsRoot + '\\' + year   # this action requires domain permission
 # on windows level
 folders = os.listdir(srcProjectRootPath)
 for i in folders:
     print(i)
+    if i.startswith(jobN):
+        projectFolder = srcProjectRootPath + '\\' + i
+        print('FOUND \n', projectFolder)
+        for j in os.listdir(projectFolder):
+            print('--- ', j)
+
+

f4dd8f1f432bae541bee4ef746ea0fdc598b2a70
Turkevich91 <vetalturkevich@gmail.com>
2020-04-27 13:52:33 -0400

    name simplifier improved accuracy cover with function, added else statement

--- fileRenamer.py
+++ fileRenamer.py
@@ -1,17 +1,25 @@
 import os
-from pathlib import Path
 
-path = r'P:\_Programming_JOBS\_Turret Punch_\2018\1873 Reston Office Bldg 3 & Garage\PAP 02'
-print(os.listdir(path))
-os.chdir(path)  # получает список файлов в папке
+Path = r'P:\_Programming_JOBS\_Turret Punch_\2018\1873 Reston Office Bldg 3 & Garage\PAP 02'
+print('укажите путь к папке:')
+Path = input()
 
-for f in [f for f in os.listdir(path) if f.lower().endswith('.dwg')]:
-    fPath = os.path.join(path, f)
-    if len(f.split('-')) == 3 and os.path.isfile(fPath):
-        file_name, file_ext = os.path.splitext(f)   # Делим файл на имя и расширение
-        f_job, f_release, f_num = file_name.split('-')  # Делим имя файла по метке "-" на 3 части
-        newName = ('{}-{}{}'.format(f_release, f_num, file_ext))
-        # print(newName)
-        print(f, ' > ', newName)
-        # os.rename(f, newName)
 
+def file_rename(path):
+    print(os.listdir(path))
+    os.chdir(path)  # Смена текущей директории
+
+    for f in [f for f in os.listdir(path) if f.lower().endswith('.dwg')]:
+        print(f)
+        if len(f.split('-')) == 3 and os.path.isfile(f):
+            file_name, file_ext = os.path.splitext(f)  # Делим файл на имя и расширение
+            f_job, f_release, f_num = file_name.split('-')  # Делим имя файла по метке "-" на 3 части
+            new_name = ('{}-{}{}'.format(f_release, f_num, file_ext))
+            # print(newName)
+            print(f, ' > ', new_name)
+            # os.rename(f, newName) # Раскоментировать перед применением
+        else:
+            print('--== change is not necessary ==--')
+
+
+file_rename(Path)
--- filesCopier.py
+++ filesCopier.py
@@ -1,6 +1,6 @@
 import os
 
-projectRoot = r"\\mcp-fsvs2\Engineering\01 Projects"
+projectsRoot = r"\\mcp-fsvs2\Engineering\01 Projects"
 workRoot = r"\\mcp-fsvs2\Production\_Programming_JOBS\_Turret Punch_"
 
 print('write work and release number using through space separator:')
@@ -9,7 +9,7 @@ jobN, releaseN = input().split()
 print('Job# ', jobN, 'Release: ', releaseN)
 year = '20' + jobN[:2]  # 2018  (first 2 digits represent year)
 print('Year: ', year)
-projectYear = os.path.join(projectRoot, year)    # this action requires domain permission
+projectYear = os.path.join(projectsRoot, year)    # this action requires domain permission
 # on windows level
 folders = os.listdir(projectYear)
 for i in folders:
@@ -21,6 +21,3 @@ for i in folders:
             print('--- ', j)
 
 print(projectFolder)
-print(os.listdir(os.path.join(workRoot, year)))
-
-

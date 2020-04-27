685560a3e22fa86dfb7ffbe2781354b066d63e7b
Turkevich91 <vetalturkevich@gmail.com>
2020-04-27 15:09:43 -0400

    file copier added 2 loops and simplified code

--- filesCopier.py
+++ filesCopier.py
@@ -5,12 +5,13 @@ workRoot = r"\\mcp-fsvs2\Production\_Programming_JOBS\_Turret Punch_"
 
 print('write work and release number using through space separator:')
 # jobN, releaseN = '1873 PAP14'.split()  #
-jobN, releaseN = input().split()
+jobN, releaseN = input('1873 PAP14').split()
 print('Job# ', jobN, 'Release: ', releaseN)
 year = '20' + jobN[:2]  # 2018  (first 2 digits represent year)
 print('Year: ', year)
-projectYear = os.path.join(projectsRoot, year)    # this action requires domain permission
-# on windows level
+os.chdir(projectsRoot)
+os.chdir(year)
+projectYear = os.getcwd()    # The current working directory to variable
 folders = os.listdir(projectYear)
 for i in folders:
     print(i)
@@ -19,5 +20,11 @@ for i in folders:
         print('FOUND \n', projectFolder)
         for j in os.listdir(projectFolder):
             print('--- ', j)
+            if j == "ID":
+                for k in os.listdir(os.path.join(projectFolder, j)):
+                    print('--- --- ', k)
+            if j == "PD":
+                for k in os.listdir(os.path.join(projectFolder, j)):
+                    print('--- --- ', k)
 
 print(projectFolder)

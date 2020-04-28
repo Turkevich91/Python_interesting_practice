e67f377ef3a71c839dd7df0801f2d48dad701b23
vetal <3316806438Tvv>
2020-04-27 22:42:57 -0400

    reduce code

--- filesCopier.py
+++ filesCopier.py
@@ -5,26 +5,22 @@ workRoot = r"\\mcp-fsvs2\Production\_Programming_JOBS\_Turret Punch_"
 
 print('write work and release number using through space separator:')
 # jobN, releaseN = '1873 PAP14'.split()  #
-jobN, releaseN = input('1873 PAP14').split()
-print('Job# ', jobN, 'Release: ', releaseN)
-year = '20' + jobN[:2]  # 2018  (first 2 digits represent year)
-print('Year: ', year)
+job, release = input('example: \n1873 PAP14').split()
+year = '20' + job[:2]  # 2018  (first 2 digits represent year)
+print('Job# ', job, 'Release: ', release, "\n" 'Year: ', year)
 os.chdir(projectsRoot)
 os.chdir(year)
-projectYear = os.getcwd()    # The current working directory to variable
-folders = os.listdir(projectYear)
-for i in folders:
+# os.chdir(r'C:\Sandbox\vetal\DefaultBox\user\current\AppData\Local\Microsoft\Windows\Explorer')
+for i in os.listdir():   # Siking
     print(i)
-    if i.startswith(jobN):
-        projectFolder = os.path.join(projectYear, i)
-        print('FOUND \n', projectFolder)
-        for j in os.listdir(projectFolder):
+    if i.startswith(job) and os.path.isdir(i):
+        jobFolder = os.path.join(os.getcwd(), i)
+        print('FOUND \n', jobFolder)
+        for j in os.listdir(jobFolder):
             print('--- ', j)
             if j == "ID":
-                for k in os.listdir(os.path.join(projectFolder, j)):
+                for k in os.listdir(os.path.join(jobFolder, j)):
                     print('--- --- ', k)
             if j == "PD":
-                for k in os.listdir(os.path.join(projectFolder, j)):
+                for k in os.listdir(os.path.join(jobFolder, j)):
                     print('--- --- ', k)
-
-print(projectFolder)

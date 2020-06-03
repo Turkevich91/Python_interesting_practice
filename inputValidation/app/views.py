a33d9c383d828cf00912ef0c5dfb839d5db77f1b
Turkevich91 <vetalturkevich@gmail.com>
2020-06-03 14:51:03 -0400

    Django app views.py

--- inputValidation/app/views.py
+++ inputValidation/app/views.py
@@ -8,10 +8,11 @@ from .models import Project, Release, Panel
 
 def index(request):
     project_numbers_list = Project.objects.order_by('project_number')
+    # quantity_of_releases = Release.project.get_foreign_related_value(project_numbers_list)
     return render(request, 'list.html', {'project_list': project_numbers_list})
 
 
-# def projects(request, project_number):
+# def releas(request, project_number):
 #     try:
 #         a = Project.objects.get(project_number=project_number)
 #     except:

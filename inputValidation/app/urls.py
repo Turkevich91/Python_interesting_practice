12da81498d308b1e806493a4da2fd56c0d9f7747
Turkevich91 <vetalturkevich@gmail.com>
2020-06-04 09:21:14 -0400

    Django app views.py

--- inputValidation/app/urls.py
+++ inputValidation/app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('production/', views.production, name='production'),
+    path('manage/', views.manage, name='manage'),
+    path('engineering/', views.engineering, name='engineering')
     # path('<int:rel_id/>', views.projects, name='Project'),
 ]
--- inputValidation/app/views.py
+++ inputValidation/app/views.py
@@ -6,16 +6,29 @@ from django.shortcuts import render
 from .models import Project, Release, Panel
 
 
-def index(request):
+def production(request):
     project_numbers_list = Project.objects.order_by('project_number')
     # quantity_of_releases = Release.project.get_foreign_related_value(project_numbers_list)
-    return render(request, 'list.html', {'project_list': project_numbers_list})
+    return render(request, 'production.html', {'project_list': project_numbers_list})
 
 
-# def releas(request, project_number):
+def manage(request):
+
+    return render(request, 'manage.html')
+
+
+def engineering(request):
+    return render(request, 'engineering.html', )
+
+
+def index(request):
+    return render(request, 'index.html')
+
+# def releases(request, project_number):
 #     try:
 #         a = Project.objects.get(project_number=project_number)
 #     except:
 #         raise Http404('Page not found')
-#
+
 #     return render(request, 'app/detail.html', {''})
+#

898839207186603bfa85f1e1859542c3bda6d78a
Vetal Turkevich <vetalturkevich@gmail.com>
2020-08-05 10:59:26 -0400

    modules.py

--- work_processes_organization/common/modules.py
+++ work_processes_organization/common/modules.py
@@ -1,20 +1,24 @@
 import openpyxl
 from openpyxl.utils import get_column_letter, column_index_from_string
 import os
+from os.path import isfile, join, splitext
 
-SCHEDULES = r"\\mcp-fsvs2\Schedules"
+SCHEDULES_FOLDER = r"\\mcp-fsvs2\Schedules"
 
 
-def grab_excel(SCHEDULES=SCHEDULES, filename=None):
+def grab_schedule(path=SCHEDULES_FOLDER, filename=None):
+    """этот модуль собирает данные с таблиц и добавляет их в базу данных.
+    Без аргументов метод возвращает список файлов .xlsx с папки SCHEDULES
+    Если передать аргумент filename то скопирует содержимое файла в базу данных.
+    """
     if filename:
-        pass
+        print(f'You are going to open file {filename}')
     else:
-        return os.listdir(SCHEDULES)
+        files = os.listdir(path)
+        return [x for x in files if isfile(join(path, x)) and not x.startswith('~') and x.endswith('.xlsx')]
 
 
-for i in [x for x in grab_excel() if os.path.isfile(os.path.join(SCHEDULES, x))]:
-    if i.startswith('~$') \
-            or i.endswith('.db',) or i.endswith('.tmp'):
-        continue
-    elif os.path.splitext(i)[1] == '.xlsx':
-        print(i)
+# for line in grab_excel(filename='Installation Schedule - 2020.xlsx'):
+#     print(line)
+
+grab_schedule(filename='Installation Schedule - 2020.xlsx')
